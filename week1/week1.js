e32a3d92138b952aaffded99bc9b006755f0a6d0
nigel-lewis <63433301+nigel-lewis@users.noreply.github.com>
2020-04-25 11:55:01 -0700

    grocery

---
+++ week1/week1.js
@@ -0,0 +1,21 @@
+var str="5"
+var number=5
+var boolean=true
+function money() {
+    
+}
+var person={
+    groceryCart:["milk","juice","meat"],
+    firstName:"H",
+    age:5,
+    isAlone:false,
+    fullName:function (firstName) {console.log()
+    
+    }
+}
+var array=[]
+console.log(person)
+if (str>number) {console.log(str)
+} else if (number>str) {console.log(number)
+} else {console.log(str+number)
+}
