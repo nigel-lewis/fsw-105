8752f71878445b6504ee364b4b324fd4a95f9473
nigel-lewis <63433301+nigel-lewis@users.noreply.github.com>
2020-05-01 10:09:18 -0700

    loops_and_arrays

---
+++ week3/week3.js
@@ -0,0 +1,31 @@
+var officeItems = ["sampler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+var count= 0
+for(var i = 0; i < officeItems.length; i++){
+    if(officeItems[i] === "computer"){
+        count++
+    }
+}
+console.log(count)
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "mike",
+        age: 12,
+        gender: "male"
+    },{
+        name: "madeline",
+        age: 80,
+        gender: "female"
+    },{
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    },{
+        name: "Sam",
+        age: 30,
+        gender: "female"
+    }
+]
+for(var j = 0; j < peopleWhoWantToSeeMadMaxFuryRoad.legnth; j++){
+    if(peopleWhoWantToSeeMadMaxFuryRoad[j] % 5 === 2 )
+}
